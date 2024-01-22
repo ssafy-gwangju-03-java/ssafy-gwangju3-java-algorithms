5c9f8aa0a9a687038d1f8fd8d03db348963b4f8f
Garam Gim <garamgim@gmail.com>
2024-01-22 11:54:28 +0900

    B 10171

---
+++ week_01_백준새싹문제/김가람/B_04_10171.java
@@ -0,0 +1,8 @@
+public class B_04_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
