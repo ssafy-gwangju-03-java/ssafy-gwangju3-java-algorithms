33cef3921e891c2e54de0adfce7d2e5a8ba961f2
Garam Gim <garamgim@gmail.com>
2024-01-22 11:41:28 +0900

    B 11382

---
+++ week_01_백준새싹문제/김가람/B_12_11382.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class B_12_11382 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a, b, c;
+        a = scanner.nextLong();
+        b = scanner.nextLong();
+        c = scanner.nextLong();
+        System.out.println(a+b+c);
+        scanner.close();
+    }
+}
