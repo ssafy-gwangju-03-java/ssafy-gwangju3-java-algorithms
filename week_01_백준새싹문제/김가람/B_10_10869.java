649606f29217ea06f3c25a890564905759041ec7
Garam Gim <garamgim@gmail.com>
2024-01-22 11:41:09 +0900

    B 10869

---
+++ week_01_백준새싹문제/김가람/B_10_10869.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class B_10_10869 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        scanner.close();
+    }
+}
