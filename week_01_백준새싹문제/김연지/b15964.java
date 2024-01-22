09bcb21095d7d7e67688513c33e7b37842c2779d
김연지 <150763254+yjkim9497@users.noreply.github.com>
2024-01-22 14:38:19 +0900

    Add files via upload
    백준2378번

---
+++ week_01_백준새싹문제/김연지/b15964.java
@@ -0,0 +1,19 @@
+package baekjoon;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class b15964 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        System.out.println((a+b)*(a-b));
+
+		
+
+	}
+
+}
