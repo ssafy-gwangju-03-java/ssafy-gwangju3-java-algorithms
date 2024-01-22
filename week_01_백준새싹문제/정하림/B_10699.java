67d468fd6465a3dfc997e1b60ca059c0132a0768
souffle1903 <121501361+souffle1903@users.noreply.github.com>
2024-01-22 14:58:42 +0900

    Add files via upload

---
+++ week_01_백준새싹문제/정하림/B_10699.java
@@ -0,0 +1,9 @@
+package W1;
+import java.time.*;
+
+public class B_10699 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now(ZoneId.of("Asia/Seoul"));
+        System.out.println(today);
+    }
+}
---
+++ week_01_백준새싹문제/정하림/B_25083.java
@@ -0,0 +1,12 @@
+package W1;
+
+public class B_25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
