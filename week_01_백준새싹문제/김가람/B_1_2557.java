7ff85f6f48a5a9bb2839b5303df9f91120a6396a
Garam Gim <garamgim@gmail.com>
2024-01-22 11:57:40 +0900

    Delete renamed files

--- week_01_백준새싹문제/김가람/B_1_2557.java
+++
@@ -1,6 +0,0 @@
-public class B_1_2557 {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		// Welcome
-	}
-}
--- week_01_백준새싹문제/김가람/B_3_7287.java
+++
@@ -1,6 +0,0 @@
-public class B_3_7287 {
-    public static void main(String[] args) {
-        System.out.println(138);
-        System.out.println("garamgim");
-    }
-}
