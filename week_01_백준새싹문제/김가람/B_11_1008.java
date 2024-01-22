ef4b98ac49f38b35f13aa16467a0a1e8b0b47aaa
Garam Gim <garamgim@gmail.com>
2024-01-22 11:41:17 +0900

    B 1008

---
+++ week_01_백준새싹문제/김가람/B_11_1008.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class B_11_1008 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a, b;
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        System.out.println(a / b);
+        scanner.close();
+    }
+}
