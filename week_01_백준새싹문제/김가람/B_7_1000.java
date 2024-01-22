6c3e059995c7da7681b9a1e12965a335ca82471a
Garam Gim <garamgim@gmail.com>
2024-01-22 11:40:37 +0900

    B 1000

---
+++ week_01_백준새싹문제/김가람/B_7_1000.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class B_7_1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a + b);
+        scanner.close();
+    }
+}
