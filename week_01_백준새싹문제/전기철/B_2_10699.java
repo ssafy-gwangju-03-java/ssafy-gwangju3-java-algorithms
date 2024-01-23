65bc2392266000869c315f4fec2068542a0cfbce
gicheol <57222722+jbs3047@users.noreply.github.com>
2024-01-23 10:45:06 +0900

    B_2_10699

---
+++ week_01_백준새싹문제/전기철/B_2_10699.java
@@ -0,0 +1,13 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class B_2_10699 {
+    public static void main (String [] args) {
+		Date date = new Date();
+		DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
+		dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println(dateFormat.format(date));
+    }
+}
