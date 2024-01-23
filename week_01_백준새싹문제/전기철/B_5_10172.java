f2bda279df3c3bdae70a3b9c19904043e0229115
Joonhyk Song <jundonghuen@naver.com>
2024-01-23 11:24:49 +0900

    B_5_10172

---
+++ week_01_백준새싹문제/전기철/B_5_10172.java
@@ -0,0 +1,11 @@
+public class B_5_10172 {
+    public static void main (String [] args) {
+        // Welcome
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+    }
+}
