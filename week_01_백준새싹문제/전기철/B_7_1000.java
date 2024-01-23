51e17b6ac2ee59a6c8776d2613755ca345a5d8df
Joonhyk Song <jundonghuen@naver.com>
2024-01-23 12:01:44 +0900

    B_7_1000

---
+++ week_01_백준새싹문제/전기철/B_7_1000.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class B_7_1000 {
+	public static void main (String [] args) {
+        // Welcome
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(a+b);
+		sc.close();
+    }
+}
