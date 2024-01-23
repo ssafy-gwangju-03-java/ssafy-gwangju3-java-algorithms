add4c86816eb4e04da3185e641b2e6e807ee9277
Joonhyk Song <jundonghuen@naver.com>
2024-01-23 12:09:03 +0900

    B_9_10998

---
+++ week_01_백준새싹문제/전기철/B_9_10998.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class B_9_10998 {
+	public static void main (String [] args) {
+        // Welcome
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(a*b);
+		sc.close();
+    }
+}
