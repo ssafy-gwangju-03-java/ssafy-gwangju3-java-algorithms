d2f1db6047bd1ebf041c40246a9487ab3092318a
Garam Gim <garamgim@gmail.com>
2024-01-23 09:07:10 +0900

    B 13 1330

---
+++ week_01_백준새싹문제/김가람/B_13_1330.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class B_13_1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+        sc.close();
+    }
+}
