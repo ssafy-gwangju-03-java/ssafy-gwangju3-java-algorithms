60ce97d7e701bef097b7ea1594593c77fa7fc9f7
Garam Gim <garamgim@gmail.com>
2024-01-23 09:26:11 +0900

    B 15 14681

---
+++ week_01_백준새싹문제/김가람/B_15_14681.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class B_15_14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        if (a > 0 && b > 0) {
+            System.out.println(1);
+        } else if (a < 0 && b > 0) {
+            System.out.println(2);
+        } else if (a < 0 && b < 0) {
+            System.out.println(3);
+        } else if (a > 0 && b < 0) {
+            System.out.println(4);
+        }
+        sc.close();
+    }
+}
