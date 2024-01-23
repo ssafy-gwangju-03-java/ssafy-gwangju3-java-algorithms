c103b47ca217acd23bc59cf6dff83ca96ce5645e
Joonhyk Song <jundonghuen@naver.com>
2024-01-23 15:45:24 +0900

    B 17 2420

---
+++ week_01_백준새싹문제/전기철/B_17_2420.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class B_17_2420 {
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		long a,b;
+		a = sc.nextLong();
+		b = sc.nextLong();
+		System.out.println(Math.abs(a-b));
+		sc.close();
+	}
+}
