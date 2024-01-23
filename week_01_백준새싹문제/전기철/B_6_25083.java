54de9dbec5e76c38637bb472894fad0a5709f54b
Joonhyk Song <jundonghuen@naver.com>
2024-01-23 11:27:28 +0900

    B_6_25083

---
+++ week_01_백준새싹문제/전기철/B_6_25083.java
@@ -0,0 +1,12 @@
+public class B_6_25083 {
+	public static void main (String [] args) {
+        // Welcome
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+		
+    }
+}
