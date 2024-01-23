64bdad1c665f96af017359e2cb2f2ef1176e3061
gicheol <57222722+jbs3047@users.noreply.github.com>
2024-01-23 11:21:18 +0900

    B_4_10171

---
+++ week_01_백준새싹문제/전기철/B_4_10171.java
@@ -0,0 +1,10 @@
+public class B_4_10171 {
+    public static void main (String [] args) {
+        // Welcome
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+    }
+}
