9bced25350d2519567cb73d49f11ecc05df4d620
Jaewooooon <wefhio1985@gmail.com>
2024-01-25 08:52:04 +0900

    B 07 1000

---
+++ week_01_백준새싹문제/최재원/B_07_1000.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class B_07_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1 + num2);
+    }
+}
