291bb41c5c6b94e4d7eac6a5b158340a73d5ba36
Joonhyk Song <jundonghuen@naver.com>
2024-01-24 15:55:03 +0900

    B 30 11654

---
+++ week_01_백준새싹문제/전기철/B_30_11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class B_30_11654 {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String s = sc.nextLine();
+	int x = s.charAt(0);
+	System.out.println(x);
+	sc.close();
+}
+}
