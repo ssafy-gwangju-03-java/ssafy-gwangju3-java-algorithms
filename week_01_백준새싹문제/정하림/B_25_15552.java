d2bfb5b6721c722504484bbc7823d9afc98d1a00
souffle1903 <121501361+souffle1903@users.noreply.github.com>
2024-01-24 17:52:45 +0900

    B 31 2743

---
+++ week_01_백준새싹문제/정하림/B_25_15552.java
@@ -0,0 +1,10 @@
+package W1;
+
+import java.io.*;
+public class B_25_15552 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ week_01_백준새싹문제/정하림/B_30_11654.java
@@ -0,0 +1,15 @@
+package W1;
+
+import java.util.Scanner;
+
+public class B_30_11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 스캐너로 char 받기 sc.next().charAt(0);
+        char ch = sc.next().charAt(0);
+        int ascii = (int) ch;
+        System.out.println(ascii);
+
+    }
+}
