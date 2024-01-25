6640f6c2cf34139bc829d7b4c0d472080d0182d2
Garam Gim <garamgim@gmail.com>
2024-01-25 16:42:23 +0900

    B 28 5597

--- week_01_백준새싹문제/김가람/B_28_5597.java
+++ week_01_백준새싹문제/김가람/B_28_5597.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class B_28_5597 {
     public static void main(String[] args) {
-        System.out.println("hello~ java~~~~~~");
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[30];
+        for (int i = 0; i < 28; i++) {
+            int student = sc.nextInt();
+            arr[student-1] = 1;
+        }
+        for (int i = 0; i < 30; i++) {
+            if (arr[i] == 0) {
+                System.out.println(i+1);
+            }
+        }
     }
 }
