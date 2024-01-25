69a4565acf7d3f4da171c4f501ea370bc10c232d
Jaewooooon <wefhio1985@gmail.com>
2024-01-25 16:18:43 +0900

    B 11 1008

---
+++ week_01_백준새싹문제/최재원/B_11_1008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class B_11_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+
+        System.out.println(A / B);
+    }
+}
