fc10ab54072a7245d9e198e603d07091baeaa895
Garam Gim <garamgim@gmail.com>
2024-01-25 10:29:38 +0900

    B 27 10807

--- week_01_백준새싹문제/김가람/B_27_10807.java
+++ week_01_백준새싹문제/김가람/B_27_10807.java
@@ -5,9 +5,11 @@ import java.io.InputStreamReader;
 public class B_27_10807 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int n = Integer.parseInt(br.readLine());
         String s = br.readLine();
         String[] arr = s.split(" ");
+
         int v = Integer.parseInt(br.readLine());
         int count = 0;
 
@@ -16,7 +18,7 @@ public class B_27_10807 {
                 count++;
             }
         }
-
+        
         System.out.println(count);
     }
 }
