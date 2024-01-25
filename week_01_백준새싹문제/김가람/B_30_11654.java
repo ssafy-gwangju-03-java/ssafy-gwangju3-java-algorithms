d966cd735497a170d967daa6669d33401ebfcfce
Garam Gim <garamgim@gmail.com>
2024-01-25 17:20:31 +0900

    B 30 11654

---
+++ week_01_백준새싹문제/김가람/B_30_11654.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class B_30_11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.next().charAt(0);
+        System.out.println(c);
+    }
+}
