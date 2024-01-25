d51b4de8948386a1f1c3b16c34ebc3cbc3f8dedd
Garam Gim <garamgim@gmail.com>
2024-01-26 08:59:24 +0900

    B 34 27866

---
+++ week_01_백준새싹문제/김가람/B_34_27866.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class B_34_27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = sc.nextInt();
+        System.out.println(s.charAt(n-1));
+    }
+}
