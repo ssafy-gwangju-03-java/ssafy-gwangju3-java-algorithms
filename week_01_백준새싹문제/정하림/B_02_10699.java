a33a326458df2933c74dfbae2fc4a867200dcc47
souffle1903 <121501361+souffle1903@users.noreply.github.com>
2024-01-26 08:55:33 +0900

    Delete

--- week_01_백준새싹문제/정하림/B_02_10699.java
+++
@@ -1,9 +0,0 @@
-package W1;
-import java.time.*;
-
-public class B_02_10699 {
-    public static void main(String[] args) {
-        LocalDate today = LocalDate.now(ZoneId.of("Asia/Seoul"));
-        System.out.println(today);
-    }
-}
--- week_01_백준새싹문제/정하림/B_1_1234.java
+++
@@ -1,5 +0,0 @@
-public class B_1_1234 {
-    public static void main (String [] args) {
-        // Welcome
-    }
-}
