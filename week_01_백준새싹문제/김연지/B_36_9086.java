e31fe4199732769205dc4842a5e15675093430e0
김연지 <150763254+yjkim9497@users.noreply.github.com>
2024-01-26 17:08:17 +0900

    B 36 9086

---
+++ week_01_백준새싹문제/김연지/B_36_9086.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B_36_9086 {
+
+	public static void main(String[] args) {
+		
+		  Scanner sc = new Scanner(System.in);
+	        int t = sc.nextInt();
+	        for(int i = 0; i < t; i++){
+	            String a = sc.next();
+	            String [] arr = a.split("");
+	            System.out.print(arr[0]);
+	            //System.out.print(arr.length);
+	             System.out.print(arr[arr.length-1]);
+	             System.out.println();
+	        }
+
+	}
+
+}
