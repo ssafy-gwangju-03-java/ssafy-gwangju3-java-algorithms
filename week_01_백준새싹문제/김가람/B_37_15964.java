c5ef2237a0d8e5251ec6a618f5d9015875df1a15
Garam Gim <garamgim@gmail.com>
2024-01-26 09:22:06 +0900

    B 37 15964

---
+++ week_01_백준새싹문제/김가람/B_37_15964.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class B_37_15964 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        System.out.println((a+b)*(a-b));
+    }
+}
