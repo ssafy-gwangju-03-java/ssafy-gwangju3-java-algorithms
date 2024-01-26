886e22870071ba64d1ea07c34ed0849adab2152f
Garam Gim <garamgim@gmail.com>
2024-01-26 09:03:44 +0900

    B 35 11718

---
+++ week_01_백준새싹문제/김가람/B_35_11718.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class B_35_11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String s = sc.nextLine();
+            System.out.println(s);
+        }
+        sc.close();
+    }
+}
