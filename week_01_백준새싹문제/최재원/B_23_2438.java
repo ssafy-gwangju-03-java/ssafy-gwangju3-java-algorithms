7a2cbc88da92bbed32e9263dbb795e90c3deb759
Jaewon Choi <wefhio1985@gmail.com>
2024-01-27 11:48:00 +0900

    B 23 2438

---
+++ week_01_백준새싹문제/최재원/B_23_2438.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_23_2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
