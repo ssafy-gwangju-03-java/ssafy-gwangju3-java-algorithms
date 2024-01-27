d143cf65ff7f43d49da99712d879072aea3d97a4
Jaewon Choi <wefhio1985@gmail.com>
2024-01-27 13:27:19 +0900

    B 30 11654

---
+++ week_01_백준새싹문제/최재원/B_30_11654.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_30_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int alphabet = br.readLine().charAt(0);
+
+        System.out.println(alphabet);
+    }
+}
