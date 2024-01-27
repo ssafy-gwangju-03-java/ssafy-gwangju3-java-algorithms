4e5e8e2cee96367b5e5df73c028857bbb90977e0
Jaewon Choi <wefhio1985@gmail.com>
2024-01-27 11:44:51 +0900

    B 22 2739

--- week_01_백준새싹문제/최재원/B_22_2739.java
+++ week_01_백준새싹문제/최재원/B_22_2739.java
@@ -1,21 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class B_20_10950 {
+public class B_22_2739 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int num = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
-
-        for (int i = 0; i < num; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            System.out.println(a + b);
+        for (int i = 1; i < 10; i++) {
+            System.out.println(num  + " * " + i + " = " + num * i);
         }
     }
 }
