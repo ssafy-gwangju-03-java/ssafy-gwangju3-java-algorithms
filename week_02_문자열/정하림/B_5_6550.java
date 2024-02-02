7baea0881aa43f48641b108ce09679bd538c9537
souffle1903 <121501361+souffle1903@users.noreply.github.com>
2024-02-02 10:47:50 +0900

    B 5 6550

--- week_02_문자열/정하림/B_5_6550.java
+++ week_02_문자열/정하림/B_5_6550.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class B_5_6550 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        // sc.hasNext()
         while (sc.hasNext()) {
             String[] s = sc.next().split("");
             String t = sc.next();
