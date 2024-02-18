15f8a5fd233c25533a3e2e904f759669c34ab25a
김연지 <150763254+yjkim9497@users.noreply.github.com>
2024-02-18 17:43:29 +0900

    B 2 2579
    포기

---
+++ week_04_DP/김연지/B_2_2579.java
@@ -0,0 +1,20 @@
+package study_week4;
+
+import java.util.Scanner;
+
+public class B_2_2579 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int [] step = new int [n];
+		for(int i = 0; i < n; i++) {
+			step[i] = sc.nextInt();
+		}
+		int total = 0;
+		
+
+	}
+
+}
