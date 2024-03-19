3868bd17efc90115a63bd89aec36ddd462bccb7d
gc <jundonghuen@naver.com>
2024-03-19 11:26:24 +0900

    B 4 2470

---
+++ week_08_투포인터/전기철/B_4_2470.java
@@ -0,0 +1,45 @@
+package week_08_투포인터.전기철;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B_4_2470 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.nextLine();
+    int[] lst = new int[n];
+    String[] x = sc.nextLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      lst[i] = Integer.parseInt(x[i]);
+    }
+    Arrays.sort(lst);
+    int chk = 2000000001;
+    int start = 0;
+    int end = n - 1;
+    int[] save = new int[2];
+    while (start < end) {
+      int sm = lst[start] + lst[end];
+      if (sm == 0) {
+        save[0] = lst[start];
+        save[1] = lst[end];
+        break;
+      }
+      if (Math.abs(chk) > Math.abs(sm)) {
+        chk = sm;
+        save[0] = lst[start];
+        save[1] = lst[end];
+      }
+      if (sm > 0) {
+        end -= 1;
+      } else {
+        start += 1;
+      }
+    }
+    System.out.print(save[0]);
+    System.out.print(" ");
+    System.out.println(save[1]);
+    sc.close();
+  }
+
+}
