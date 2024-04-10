f5e05ad01cc86f08fc23eff4d91c8b042c98ce29
JaewonChoi <wefhio1985@gmail.com>
2024-04-10 14:22:06 +0900

    B 2 20040

---
+++ week_11_유니온파인드/최재원/B_2_20040.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_2_20040 {
+    static int n, m;
+    static int[] p;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        p = makeSet();
+
+        int answer = 0;
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (findSet(a) != findSet(b)) {
+                union(a, b);
+            } else {
+                answer = i + 1;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
+
+    static void union(int a, int b) {
+        if (a < b) {
+            p[findSet(b)] = findSet(a);
+        } else {
+            p[findSet(a)] = findSet(b);
+        }
+    }
+
+    static int findSet(int x) {
+        if (x == p[x]) {
+            return x;
+        }
+
+        return p[x] = findSet(p[x]);
+    }
+
+
+    static int[] makeSet() {
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
+
+        return arr;
+    }
+}
