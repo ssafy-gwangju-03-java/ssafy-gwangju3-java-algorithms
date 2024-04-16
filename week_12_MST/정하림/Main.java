18205aa0d3df2c03f14ed8ac15c944f607eb67dd
souffle1903 <121501361+souffle1903@users.noreply.github.com>
2024-04-16 15:24:27 +0900

    B 1 16398

---
+++ week_12_MST/정하림/Main.java
@@ -0,0 +1,66 @@
+package java_study.w12.B_1_16398;
+
+import java.util.*;
+
+class Edge implements Comparable<Edge> {
+    int to, weight;
+
+    public Edge(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return this.weight - o.weight;
+    }
+}
+
+public class Main {
+    static int N;
+    static List<Edge>[] adjl;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        adjl = new ArrayList[N];
+        for (int i = 0; i < N; i++) adjl[i] = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int w = sc.nextInt();
+                adjl[i].add(new Edge(j, w));
+            }
+        }
+
+        System.out.println(prim());
+
+    }
+
+    private static long prim() {
+        Queue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(0, 0));
+
+        boolean[] MST = new boolean[N];
+        long sum = 0;
+        while (!pq.isEmpty()){
+            Edge poll = pq.poll();
+            int to = poll.to;
+            int weight = poll.weight;
+
+            if (MST[to]) continue;
+            MST[to] = true;
+            sum += weight;
+
+            for (Edge nxt : adjl[to]){
+                if (!MST[nxt.to]) pq.offer(nxt);
+            }
+        }
+
+        return sum;
+
+
+    }
+
+}
