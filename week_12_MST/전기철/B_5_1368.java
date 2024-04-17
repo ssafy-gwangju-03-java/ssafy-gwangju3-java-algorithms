0933814033969de330ff39004cc5c2dd4d24a884
gc <jundonghuen@naver.com>
2024-04-17 13:36:46 +0900

    B 5 1368

---
+++ week_12_MST/전기철/B_5_1368.java
@@ -0,0 +1,68 @@
+package week_12_MST.전기철;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class B_5_1368 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] water = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            water[i] = sc.nextInt();
+        }
+        sc.nextLine();
+
+        int[] vis = new int[n + 1];
+        long ans = 0;
+        List<int[]>[] lst = new List[n + 1];
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] s = sc.nextLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(s[j]);
+            }
+        }
+
+        for (int i = 0; i < n + 1; i++) {
+            lst[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                lst[i].add(new int[] { arr[i][j], j });
+                lst[j].add(new int[] { arr[i][j], i });
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            lst[n].add(new int[] { water[i], i });
+            lst[i].add(new int[] { water[i], n });
+        }
+        
+        PriorityQueue<int[]> heap = new PriorityQueue<>(Comparator.comparingInt(k -> k[0]));
+        heap.add(new int[] { 0, 0 });
+        while (!heap.isEmpty()) {
+            int[] edge = heap.poll();
+            int w = edge[0];
+            int now = edge[1];
+            if (vis[now] == 0) {
+                vis[now] = 1;
+                ans += w;
+                for (int[] next : lst[now]) {
+                    if (vis[next[1]] == 0) {
+                        heap.add(next);
+                    }
+                }
+            }
+
+        }
+        System.out.println(ans);
+        sc.close();
+    }
+}
